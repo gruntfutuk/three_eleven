37a19dff1bc730d5d99fcfeebd5486baba0d63db
Grunt Futuk <gruntfutuk@users.noreply.github.com>
2022-12-18 17:12:44 +0000

    misc updates and additions

--- yes_no.py
+++ yes_no.py
@@ -1,12 +1,17 @@
-    def is_yes(prompt):
-        while True:
-            response = input(prompt).strip().casefold()
-            if response in AFFIRMATION:
-                return True
-            if response in REJECTION:
-                return False
-            print('Sorry, I did not understand that')
+def is_yes(prompt:str="Yes or No?", default:bool=None) -> bool:
+    """ prompt user and only accept answer that is in AFFIRMATION or REJECTION sets
+    and return True or False accordingly; return alone can be accepted if default
+    set to either True or False in which case the default is returned """
+    while True:
+        response = input(prompt).strip().casefold()
+        if not response and default is not None:
+            return default
+        if response in AFFIRMATION:
+            return True
+        if response in REJECTION:
+            return False
+        print('Sorry, I did not understand that')
 
 
-    AFFIRMATION = frozenset(('y', 'yes', 'yup', 'yeh', 'ok', '1'))
-    REJECTION = frozenset(('n', 'no', 'nah', 'nope', '0'))
+AFFIRMATION = frozenset(('y', 'yes', 'yup', 'yeh', 'ok', '1'))
+REJECTION = frozenset(('n', 'no', 'nah', 'nope', '0'))
